76ca8fbe4e4b78903c444d2c1c09183257d0216a
Aleksandra Mihel <mihelaleksa@yandex.ru>
2024-10-30 18:54:24 +0400

    Написала скрипт добавления карточек и удаления

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM-узлы
+const cardContainer = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(cardData, cardDelete) {
+  // Клонируем содержимое шаблона
+  const cardElement = cardTemplate.cloneNode(true);
+  
+  // Устанавливаем значения для карточки
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
 
-// @todo: Функция удаления карточки
+  // Добавляем обработчик для кнопки удаления
+  deleteButton.addEventListener('click', cardDelete);
+  
+  return cardElement;
+}
 
-// @todo: Вывести карточки на страницу
+// Функция для удаления карточки
+function cardDelete(evt) {
+  evt.target.closest('.card').remove();
+}
+
+// Выводим карточки на страницу
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData, cardDelete);
+  cardContainer.append(cardElement);
+});
